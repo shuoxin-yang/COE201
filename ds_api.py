f29d6441739cf2b677ddd7eb55b9243ba990c339
momo <2370230957@qq.com>
2026-05-01 15:14:55 +0800

    ds_api

---
+++ ds_api.py
@@ -0,0 +1,21 @@
+# Please install OpenAI SDK first: `pip3 install openai`
+import os
+from openai import OpenAI
+
+client = OpenAI(
+    api_key="sk-3923a5476164418da4324c6fefa9535c",
+    base_url="https://api.deepseek.com",
+)
+
+response = client.chat.completions.create(
+    model="deepseek-v4-pro",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant"},
+        {"role": "user", "content": "教我线性代数"},
+    ],
+    stream=False,
+    reasoning_effort="high",
+    extra_body={"thinking": {"type": "enabled"}},
+)
+
+print(response.choices[0].message.content)
